3ff109529fd2b4039a33da4c465a0d1e320ce1b0
Koi Den <prostobydet@gmail.com>
2021-06-07 21:44:53 +0300

    Добавьте файлы проекта.

---
+++ TestAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace TestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var ApiKey = "32e7ef7473524a64fee2f7f42b66389d";
+            //var City = "Kazan";
+            var url = $"http://api.icndb.com/jokes/random/3";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                //Console.WriteLine(result);
+                var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(weatherForecast.value[0].joke);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ TestAPI/RootCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPI
+{
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Value
+        {
+            public int id { get; set; }
+            public string joke { get; set; }
+            public List<object> categories { get; set; }
+        }
+
+        public class Root
+        {
+            public string type { get; set; }
+            public List<Value> value { get; set; }
+        }
+}
